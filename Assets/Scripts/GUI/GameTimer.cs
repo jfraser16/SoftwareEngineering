01d40053cdedd29c875ce3627ca788e5efbb3627
Guy-Sensei <thewarder@gmail.com>
2014-04-14 13:00:10 -0400

    Revert "YAY!!!"
    This reverts commit 3f85c0e74e1a5ba3a074323787a373f1db4d3066.

---
+++ Assets/Scripts/GUI/GameTimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer {
+
+	public GameTimer()
+	{
+
+	}
+
+
+	public void Update(float deltatime)
+	{
+		//setCurrentTime(currentTime + deltatime);
+	}
+
+	public void reset()
+	{
+		//setMaxTime(0.0f);
+		//setStartTime(0.0f);
+		//setCurrentTime(0.0f);
+	}
+
+}
---
+++ Assets/Scripts/MiniGameB/MathGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathGame : GameManager {
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        // My Awake stuff
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
